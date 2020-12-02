f4b9504e135b1d213db5c1fc3e2e967e56e27653
Dmytro Kulyk <dkulyk@users.noreply.github.com>
2020-12-02 09:25:00 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/NovaRestrictionsServiceProvider.php
+++ src/NovaRestrictionsServiceProvider.php
@@ -2,17 +2,17 @@
 
 namespace DKulyk\Restrictions;
 
-use Laravel\Nova\Nova;
-use Laravel\Nova\Events\ServingNova;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Nova\Events\ServingNova;
+use Laravel\Nova\Nova;
 
 final class NovaRestrictionsServiceProvider extends ServiceProvider
 {
     public function boot()
     {
         Event::listen(ServingNova::class, function () {
-            Nova::script('dkulyk-restrictions', dirname(__DIR__) . '/dist/js/restrictions.js');
+            Nova::script('dkulyk-restrictions', dirname(__DIR__).'/dist/js/restrictions.js');
         });
     }
 }
